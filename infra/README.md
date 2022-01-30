3ce5f4271566acbfe2d33041dd3fe9b1b1536ab4
XEROINSIDE\ryan.achten <ryan.achten@xero.com>
2022-01-31 12:12:44 +1300

    Add working TF deployment

--- infra/README.md
+++ infra/README.md
@@ -4,7 +4,8 @@
 ```
 $env:TF_VAR_heroku_api_key="key created in Heroku"
 $env:TF_VAR_heroku_email="email associated with Heroku account"
-
+$env:TF_VAR_unsplash_access_key="Unsplash API access key"
+$env:TF_VAR_unsplash_secret_key="Unsplash API secret key"
 ```
 - Initialise TF: `terraform init`
 - Run TF plan to ensure resources to be created are correct: `terraform plan`
--- infra/variables.tf
+++ infra/variables.tf
@@ -1,18 +1,29 @@
-# Replaced by secret
+variable "heroku_app_name" {
+  description = "Heroku app name (lower case)"
+  type        = string
+  default     = "thatsagoodboy"
+}
+
+#### SECRETS - Replaced by process environment variables ####
 variable "heroku_email" {
   description = "Email address of Heroku account"
   type        = string
 }
 
-# Replaced by secret
 variable "heroku_api_key" {
   description = "API key for Heroku account"
   type        = string
   sensitive   = true
 }
 
-variable "heroku_app_name" {
-  description = "Heroku app name (lower case)"
+variable "unsplash_access_key" {
+  description = "Access key for Unsplash"
   type        = string
-  default     = "thatsagoodboy"
+  sensitive   = true
+}
+
+variable "unsplash_secret_key" {
+  description = "Secret key for Unsplash"
+  type        = string
+  sensitive   = true
 }
