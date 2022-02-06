5bb5ca0af567f4ff6f8099ab836792356f6c23f3
RyanAchten <ryanachten@gmail.com>
2022-02-06 16:18:35 +1300

    Add site meta, Consolidate app init

--- app/pages/_app.tsx
+++ app/pages/_app.tsx
@@ -1,7 +1,15 @@
 import "../styles/globals.scss";
 import type { AppProps } from "next/app";
+import React from "react";
+import useClassificationSetup from "../hooks/useClassificationSetup";
+import LoadingSplash from "../components/LoadingSplash";
 
 function MyApp({ Component, pageProps }: AppProps) {
+  const loading = useClassificationSetup();
+
+  if (loading) {
+    return <LoadingSplash />;
+  }
   return <Component {...pageProps} />;
 }
 
--- app/pages/upload.tsx
+++ app/pages/upload.tsx
@@ -1,24 +1,20 @@
 import type { NextPage } from "next";
+import Head from "next/head";
 import React, { useState } from "react";
 import FileUpload from "../components/FileUpload";
 import ImageClassification from "../components/ImageClassification";
-import LoadingSplash from "../components/LoadingSplash";
 import PageHeader from "../components/PageHeader";
-import useClassificationSetup from "../hooks/useClassificationSetup";
 
 import pageStyles from "../styles/Page.module.scss";
 import styles from "../styles/Upload.module.scss";
 
 const Upload: NextPage = () => {
   const [image, setImage] = useState("");
-  const loading = useClassificationSetup();
-
-  if (loading) {
-    return <LoadingSplash />;
-  }
-
   return (
     <main className={pageStyles.wrapper}>
+      <Head>
+        <title>whosagoodboy | Upload</title>
+      </Head>
       <PageHeader />
       <div className={styles.content}>
         <FileUpload onImageSelected={(uri) => setImage(uri)} />
